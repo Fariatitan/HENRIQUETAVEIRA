c59f7b63331a5fe5bcb07af33a1a8155d0885660
Fariatitan <henrique950704952@gmail.com>
2026-09-15 13:07:16 -0300

    Delete src directory

--- src/static-main.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import StaticApp from '@/StaticApp';
-
-createRoot(document.getElementById('root')).render(<StaticApp />);
